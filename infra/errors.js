537073a7fe4a80ea22b6198c88dcf074dc465d85
Adriano Garcia <adrianogtl@protonmail.com>
2026-07-17 13:07:35 +0000

    feat: implement session creation

--- infra/errors.js
+++ infra/errors.js
@@ -79,6 +79,27 @@ export class ValidationError extends Error {
   }
 }
 
+export class UnauthorizedError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "User not authenticated", {
+      cause,
+    });
+
+    this.name = "UnauthorizedError";
+    this.action = action || "Try login again.";
+    this.statusCode = 401;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class NotFoundError extends Error {
   constructor({ cause, message, action }) {
     super(message || "We couldn't find what you were looking for.", {
