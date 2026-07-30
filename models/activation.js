657cf66702c1d3c2b64add7555e528ecea73b700
Adriano Garcia <adrianogtl@protonmail.com>
2026-07-30 15:17:46 +0000

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -2,7 +2,8 @@ import database from "infra/database.js";
 import email from "infra/email.js";
 import webserver from "infra/webserver.js";
 import user from "models/user.js";
-import { NotFoundError } from "infra/errors.js";
+import authorization from "./authorization.js";
+import { ForbiddenError, NotFoundError } from "infra/errors.js";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 Minutes
 
@@ -64,10 +65,20 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "You cannot use activation token anymore.",
+      action: "Contact support.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
   ]);
+
   return activatedUser;
 }
 
@@ -117,6 +128,7 @@ const activation = {
   activateUserByUserId,
   create,
   sendEmailToUser,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default activation;
